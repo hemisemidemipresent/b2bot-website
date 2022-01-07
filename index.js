1d2a74a93c4cafbb52a3901c8fd6945bfba1f034
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-01-07 23:05:26 +0800

    eyewear color

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function createString() {
     let hatColor = document.getElementById('hatcolor').value;
     let finColor = document.getElementById('fin').value;
     let eyewear = document.getElementById('eyewear').value;
+    let eyewearColor = document.getElementById('eyewearcolor').value;
+
     let mouth = document.getElementById('mouth').value;
     let accessories = document.getElementById('accessories').value;
     let accessoriesColor = document.getElementById('accessoriescolor').value;
     document.getElementById(
         'res'
-    ).innerText = `body(${bodyColor});hat(${hat},${hatColor});fin(default,${finColor});eyewear(${eyewear});mouth(${mouth});accessories(${accessories},${accessoriesColor})`;
+    ).innerText = `body(${bodyColor});hat(${hat},${hatColor});fin(default,${finColor});eyewear(${eyewear},${eyewearColor});mouth(${mouth});accessories(${accessories},${accessoriesColor})`;
 }
